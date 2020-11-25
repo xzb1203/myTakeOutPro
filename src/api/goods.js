b7ebfb16b2b138dd1aec1411b95ec899bddc2ade
萌多 <870110664@qq.com>
2020-11-25 23:30:16 +0800

    完成商品分类部分功能

--- src/api/goods.js
+++ src/api/goods.js
@@ -15,3 +15,27 @@ export function goodsDel(params) {
     params
   })
 }
+// 获取分类
+export function goodsCatelist(params) {
+  return request({
+    url: '/goods/catelist',
+    method: 'get',
+    params
+  })
+}
+// 修改分类
+export function goodsEditcate(data) {
+  return request({
+    url: '/goods/editcate',
+    method: 'post',
+    data
+  })
+}
+// 删除分类
+export function goodsDelcate(id) {
+  return request({
+    url: '/goods/delcate',
+    method: 'get',
+    params: { id }
+  })
+}
